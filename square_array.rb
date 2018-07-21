beb619e0c3480e903fa04d570491b7417245de67
Amee Covarrubias <ameecova@yahoo.com>
2018-07-21 00:56:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,11 @@
 def square_array(array)
   # your code here
+<<<<<<< HEAD
   arr = Array.new
   array.each {|i| arr << i ** 2}
   arr
+=======
+  array.each {|i| array << i ** 2}
+  array
+>>>>>>> 2e7e009d0870302aa259bdd263d55ca05c7838db
 end
